11769a4621a4c96e905e5aedcc032ec5a690e5ce
Giles Knap <gilesknap@gmail.com>
2023-03-31 14:09:44 +0100

    get adurl building

--- adurl/adurl.sh
+++ adurl/adurl.sh
@@ -4,33 +4,12 @@ echo '
 CROSS_COMPILER_TARGET_ARCHS =
 
 # Enable file plugins and source them all from ADSupport
-# this minimizes dependency issues with system libraries
 
 WITH_GRAPHICSMAGICK = YES
 GRAPHICSMAGICK_EXTERNAL = NO
 
-WITH_HDF5     = YES
-HDF5_EXTERNAL = NO
-
-WITH_SZIP     = YES
-SZIP_EXTERNAL = NO
-
 WITH_JPEG     = YES
 JPEG_EXTERNAL = NO
-
-WITH_TIFF     = YES
-TIFF_EXTERNAL = NO
-
-XML2_EXTERNAL = NO
-
-WITH_ZLIB     = YES
-ZLIB_EXTERNAL = NO
-
-WITH_BLOSC    = YES
-BLOSC_EXTERNAL= NO
-
-WITH_CBF      = YES
-CBF_EXTERNAL  = NO
 
 WITH_PVA      = YES
 WITH_BOOST    = YES
