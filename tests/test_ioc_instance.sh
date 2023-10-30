89eed36e746f05a30f5bb15de3f37c1490186bff
Giles Knap <giles.knap@diamond.ac.uk>
2023-10-30 12:21:18 +0000

    update to latest ibek

--- tests/test_ioc_instance.sh
+++ tests/test_ioc_instance.sh
@@ -15,7 +15,7 @@ ibek startup generate ioc.yaml ../../*/*.support.yaml --out ${tmpdir}/st.cmd \
     --db-out ${tmpdir}/ioc.db
 
 if ! diff ${tmpdir}/st.cmd ./st.cmd || ! diff ${tmpdir}/ioc.db ./ioc.db; then
-    echo "ERROR: Generated files st.cmd / db.subst differ from expected"
+    echo "ERROR: Generated files st.cmd / ioc.subst differ from expected"
     exit 1
 fi
 
