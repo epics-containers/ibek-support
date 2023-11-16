3fc54203cc0b0340cafd539461ede6175582a85a
Giles Knap <gilesknap@gmail.com>
2023-11-16 16:39:52 +0000

    add pvi device files for ADCore and Aravis

---
+++ pvi-generate.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+# this script is used to generate the PVI files needed to make some basic
+# auto generated GUI for currently supported Generic IOCs. In particular
+# AreaDetector ones as these are moderately complex.
+#
+# This is used as a record of the steps to take to get these files
+# created but can be retired once we are happy with the device yaml in
+# any given support module.
+#
+# note the sed commands - some of these may get fixed in pvi and not longer
+# be needed, others may be premanent manual edits.
+#
+# NOTE: what is not inlcuded in any script yet is the addition of the pvi:
+# entry into *.ibek.support.yaml. These are required in order for the
+# ibek runtime generate to turn the device yaml into bob files.
+# This means that re-running build_support.sh would delete these entries.
+
+cd dirname ${0}
+
+if [[ $(realpath $(pwd)) != /epics/*/ibek-support ]]; then
+    echo "Must be run an ibek-suppport directory inside a container"
+    exit 1
+fi
+
+if not pvi --version; then
+    echo "pvi not found"
+    echo "please clone pvi into /repos and 'pip install -e /repos/pvi'"
+    exit 1
+fi
+
+set -e
+
+ADCORE=/epics/support/ADCore
+cd ADCore
+
+pvi convert device --template ${ADCORE}/db/NDProcess.template . ${ADCORE}/ADApp/pluginSrc/NDPluginProcess.h
+pvi regroup NDPluginProcess.pvi.device.yaml ${ADCORE}/ADApp/op/adl/*.adl
+
+pvi convert device --template ${ADCORE}/db/NDStats.template . ${ADCORE}/ADApp/pluginSrc/NDPluginStats.h
+pvi regroup NDPluginStats.pvi.device.yaml ${ADCORE}/ADApp/op/adl/*.adl
+
+pvi convert device --template ${ADCORE}/db/NDROI.template . ${ADCORE}/ADApp/pluginSrc/NDPluginROI.h
+pvi regroup NDPluginROI.pvi.device.yaml ${ADCORE}/ADApp/op/adl/*.adl
+
+pvi convert device --template ${ADCORE}/db/ADBase.template . ${ADCORE}/ADApp/ADSrc/ADDriver.h
+pvi regroup ADDriver.pvi.device.yaml ${ADCORE}/ADApp/op/adl/*.adl
+
+ADGenICam=/epics/support/ADGenICam
+cd ../ADGenICam
+
+pvi convert device --template ${ADGenICam}/db/ADGenICam.template . ${ADGenICam}/GenICamApp/src/ADGenICam.h
+pvi regroup ADGenICam.pvi.device.yaml ${ADGenICam}/GenICamApp/op/adl/*.adl
+
+ADARAVIS=/epics/support/ADAravis
+cd ../ADAravis
+
+pvi convert device --template ${ADARAVIS}/db/aravisCamera.template . ${ADARAVIS}/aravisApp/src/arvFeature.h
+sed -i arvFeature.pvi.device.yaml -e s/aravisCamera/AravisCamera/
+pvi regroup arvFeature.pvi.device.yaml ${ADARAVIS}/aravisApp/op/adl/*.adl
+
+# these commands link all the device yamls into /epics/pvi-defs
+ibek support generate-links ADCore
+ibek support generate-links ADAravis
+ibek support generate-links ADGenICam
