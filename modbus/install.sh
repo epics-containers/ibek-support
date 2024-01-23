035d2d47f7a2a3cbcadecb95294a973fa3fd7246
Giles Knap <giles.knap@diamond.ac.uk>
2024-01-23 10:12:19 +0000

    update modbus to not add asyn lib

--- modbus/install.sh
+++ modbus/install.sh
@@ -18,7 +18,7 @@ ibek support git-clone ${NAME} ${VERSION}
 ibek support register ${NAME}
 
 # declare the libs and DBDs that are required in ioc/iocApp/src/Makefile
-ibek support add-libs asyn modbus
+ibek support add-libs modbus
 ibek support add-dbds modbusSupport.dbd
 
 # Patches to the CONFIG_SITE
