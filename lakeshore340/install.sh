c3c3b0917397b3c09e53bc353011679af56780a1
Subin Saji <subinssaji@gmail.com>
2024-01-31 15:50:03 +0000

    support for lakshore340 ioc

---
+++ lakeshore340/install.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+##########################################################################
+##### install script for lakeshore340 support module #########################
+##########################################################################
+
+# ARGUMENTS:
+#  $1 VERSION to install (must match repo tag)
+VERSION=${1}
+NAME=lakeshore340
+FOLDER=$(dirname $(readlink -f $0))
+
+# log output and abort on failure
+set -xe
+
+# doxygen is used in documentation build for the developer stage
+ibek support apt-install --only=dev doxygen
+
+# get the source and fix up the configure/RELEASE files
+ibek support git-clone ${NAME} ${VERSION} --org https://github.com/DiamondLightSource/
+
+ibek support register ${NAME}
+
+# declare the libs and DBDs that are required in ioc/iocApp/src/Makefile
+# None required for a stream device ------------------------------------
+#ibek support add-libs
+#ibek support add-dbds
+
+# compile the support module
+ibek support compile ${NAME}
+# prepare *.bob, *.pvi, *.ibek.support.yaml for access outside the container.
+ibek support generate-links ${FOLDER}
