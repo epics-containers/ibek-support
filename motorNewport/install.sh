55ea894966fa55268db2bb39f1f3cd65d25b7a9e
amichelo <andrea.michelotti@lnf.infn.it>
2024-05-20 23:42:06 +0200

    added motorNewport to generic support

---
+++ motorNewport/install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# ARGUMENTS:
+#  $1 VERSION to install (must match repo tag)
+VERSION=${1}
+NAME=motorNewport
+FOLDER=$(dirname $(readlink -f $0))
+
+# log output and abort on failure
+set -xe
+
+# get the source and fix up the configure/RELEASE files
+ibek support git-clone ${NAME} ${VERSION} --org http://github.com/epics-motor/
+ibek support register ${NAME}
+
+# declare the libs and DBDs that are required in ioc/iocApp/src/Makefile
+ibek support add-libs Newport motor asyn
+ibek support add-dbds devNewport.dbd motorRecord.dbd motorSupport.dbd
+# global config settings
+${FOLDER}/../_global/install.sh ${NAME}
+
+# compile the support module
+ibek support compile ${NAME}
+# prepare *.bob, *.pvi, *.ibek.support.yaml for access outside the container.
+ibek support generate-links ${FOLDER}
+
+
