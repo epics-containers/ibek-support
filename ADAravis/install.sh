2624a92b9edfea95f00d098f5308c197d1bf8b9a
amichelo <andrea.michelotti@lnf.infn.it>
2024-05-20 22:34:36 +0200

    update ibek with camera supports

--- ADAravis/install.sh
+++ ADAravis/install.sh
@@ -26,12 +26,12 @@ ibek support apt-install \
     xz-utils
 
 # declare packages for installation in the Dockerfile's runtime stage
-ibek support add-runtime-packages libglib2.0-bin libusb-1.0 libxml2
+ibek support add-runtime-packages libglib2.0-bin libusb-1.0 libxml2 aravis-tools-cli
 
 # build aravis library
 (
     cd /usr/local &&
-    git clone -b "0.8.29" --depth 1 https://github.com/AravisProject/aravis &&
+    git clone -b "0.8.31" --depth 1 https://github.com/AravisProject/aravis &&
     cd aravis &&
     meson build &&
     cd build &&
--- ADGenICam/install.sh
+++ ADGenICam/install.sh
@@ -11,9 +11,9 @@ FOLDER=$(dirname $(readlink -f $0))
 
 # log output and abort on failure
 set -xe
-
+VERSION=master
 # get the source and fix up the configure/RELEASE files
-ibek support git-clone ${NAME} ${VERSION} --org http://github.com/areaDetector/
+ibek support git-clone ${NAME} ${VERSION} --org https://github.com/amichelotti # http://github.com/areaDetector/
 ibek support register ${NAME}
 
 # declare the libs and DBDs that are required in ioc/iocApp/src/Makefile
