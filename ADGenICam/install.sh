c609bd6cd8912095c79900701ec2b91b96fc730d
amichelo <andrea.michelotti@lnf.infn.it>
2024-05-21 10:53:11 +0200

    version not fixed

--- ADGenICam/install.sh
+++ ADGenICam/install.sh
@@ -11,7 +11,6 @@ FOLDER=$(dirname $(readlink -f $0))
 
 # log output and abort on failure
 set -xe
-VERSION=master
 # get the source and fix up the configure/RELEASE files
 ibek support git-clone ${NAME} ${VERSION} --org http://github.com/areaDetector/
 ibek support register ${NAME}
