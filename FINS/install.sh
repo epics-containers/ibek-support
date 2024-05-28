4cd8c133d8a2ea869178851129988641c41633f4
Giles Knap <giles.knap@diamond.ac.uk>
2024-05-28 08:17:09 +0000

    add FINS, ether_ip

---
+++ FINS/install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# ARGUMENTS:
+#  $1 VERSION to install (must match repo tag)
+VERSION=${1}
+NAME=FINS
+FOLDER=$(dirname $(readlink -f $0))
+
+# log output and abort on failure
+set -xe
+
+# get the source and fix up the configure/RELEASE files
+ibek support git-clone ${NAME} ${VERSION} --org https://github.com/DiamondLightSource
+
+ibek support register ${NAME}
+
+# no dbds/libs for a streamdevice
+ibek support add-libs FINS
+ibek support add-dbds FINS.dbd
+
+# global config settings
+${FOLDER}/../_global/install.sh ${NAME}
+
+# compile the support module
+ibek support compile ${NAME}
+# prepare *.bob, *.pvi, *.ibek.support.yaml for access outside the container.
+ibek support generate-links ${FOLDER}
---
+++ ether_ip/install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# ARGUMENTS:
+#  $1 VERSION to install (must match repo tag)
+VERSION=${1}
+NAME=ether_ip
+FOLDER=$(dirname $(readlink -f $0))
+
+# log output and abort on failure
+set -xe
+
+# get the source and fix up the configure/RELEASE files
+ibek support git-clone ${NAME} ${VERSION}
+
+ibek support register ${NAME}
+
+# no dbds/libs for a streamdevice
+ibek support add-libs ether_ip
+ibek support add-dbds ether_ip.dbd
+
+# global config settings
+${FOLDER}/../_global/install.sh ${NAME}
+
+# compile the support module
+ibek support compile ${NAME}
+# prepare *.bob, *.pvi, *.ibek.support.yaml for access outside the container.
+ibek support generate-links ${FOLDER}
