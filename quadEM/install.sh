03485f4b3c9af61b8e75e3819ce393b306f5f5b6
Giles Knap <giles.knap@diamond.ac.uk>
2024-05-28 12:37:04 +0000

    fix quadEM

--- quadEM/install.sh
+++ quadEM/install.sh
@@ -9,11 +9,8 @@ FOLDER=$(dirname $(readlink -f $0))
 # log output and abort on failure
 set -xe
 
-# doxygen is used in documentation build for the developer stage
-ibek support apt-install doxygen
-
 # get the source and fix up the configure/RELEASE files
-ibek support git-clone ${NAME} ${VERSION} --org https://github.com/epics-modules/
+ibek support git-clone ${NAME} ${VERSION}
 
 ibek support register ${NAME}
 
