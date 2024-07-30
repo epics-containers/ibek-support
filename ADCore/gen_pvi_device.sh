9d221fe3f3a745e424de8ea101f09217d0588b6f
Giles Knap <giles.knap@diamond.ac.uk>
2024-07-30 10:20:52 +0000

    include all pvi devices in the ADCore support yaml

--- ADCore/gen_pvi_device.sh
+++ ADCore/gen_pvi_device.sh
@@ -7,6 +7,8 @@ ADCORE=/epics/support/ADCore
 
 set -e
 
+# We are not yet supporting the xxxN templates
+# the base templates are handled separately
 skip="
 NDArrayBase
 NDAttrPlotAttr
@@ -34,3 +36,17 @@ for i in $ADCORE/db/ND*.template ; do
     pvi convert device --template $i . ${ADCORE}/ADApp/pluginSrc/ND*${name#"ND"}.h
     pvi regroup ND*${name#"ND"}.pvi.device.yaml ${ADCORE}/ADApp/op/adl/*.adl
 done
+
+# handle the base templates ####################################################
+
+pvi convert device --template ${ADCORE}/db/NDArrayBase.template . ${ADCORE}/ADApp/ADSrc/asynNDArrayDriver.h
+pvi regroup asynNDArrayDriver.pvi.device.yaml ${ADCORE}/ADApp/op/adl/*.adl
+
+pvi convert device --template ${ADCORE}/db/ADBase.template . ${ADCORE}/ADApp/ADSrc/ADDriver.h
+pvi regroup ADDriver.pvi.device.yaml ${ADCORE}/ADApp/op/adl/*.adl
+
+pvi convert device --template ${ADCORE}/db/NDPluginBase.template . ${ADCORE}/ADApp/pluginSrc/NDPluginDriver.h
+pvi regroup NDPluginDriver.pvi.device.yaml ${ADCORE}/ADApp/op/adl/*.adl
+
+-rw-rw-r--. 1 root root  6293 Jun 24 11:23
+-rw-r--r--. 1 root root  4328 Mar 28 10:29
