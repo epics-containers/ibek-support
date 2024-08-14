bb98c50446c1771b6e0cf34210d76a618e549bec
Giles Knap <giles.knap@diamond.ac.uk>
2024-08-14 13:28:12 +0000

    trim pvxs

--- lakeshore340/install.sh
+++ lakeshore340/install.sh
@@ -9,25 +9,23 @@ FOLDER=$(dirname $(readlink -f $0))
 # log output and abort on failure
 set -xe
 
-# doxygen is used in documentation build for the developer stage
-ibek support apt-install doxygen
-
 # get the source and fix up the configure/RELEASE files
 ibek support git-clone ${NAME} ${VERSION} --org https://github.com/DiamondLightSource/
-
 ibek support register ${NAME}
 
 # declare the libs and DBDs that are required in ioc/iocApp/src/Makefile
-# None required for a stream device ------------------------------------
-#ibek support add-libs
-#ibek support add-dbds
+# NONE required for StreamDevice
+# ibek support add-libs
+# ibek support add-dbds
+
+# comment out the documentation from the Makefile
+sed -i -E 's/(^[^#].*documentation)/# \1/' ${SUPPORT}/${NAME}/Makefile
 
 # global config settings
-${FOLDER}/../_global/install.sh ${NAME}
+${FOLDER}/../_global/install.sh
 
 # compile the support module
 ibek support compile ${NAME}
 # prepare *.bob, *.pvi, *.ibek.support.yaml for access outside the container.
 ibek support generate-links ${FOLDER}
-
 
