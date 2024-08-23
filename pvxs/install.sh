c17d609756cc7e487245afac6018ef311ab9a5b1
Giles Knap <giles.knap@diamond.ac.uk>
2024-08-23 15:11:17 +0000

    remove pvxs for rtems

--- pvxs/install.sh
+++ pvxs/install.sh
@@ -9,29 +9,34 @@ VERSION=${1}
 NAME=pvxs
 FOLDER=$(dirname $(readlink -f $0))
 
-# log output and abort on failure
-set -xe
+if [[ $EPICS_TARGET_ARCH == "RTEMS"* ]]; then
+    echo "pvxs is not supported on RTEMS"
+else
 
-# install required system dependencies
-ibek support apt-install libevent-dev
+    # log output and abort on failure
+    set -xe
 
-# declare packages for installation in the Dockerfile's runtime stage
-ibek support add-runtime-packages libevent-dev
+    # install required system dependencies
+    ibek support apt-install libevent-dev
 
-# get the source and fix up the configure/RELEASE files
-ibek support git-clone ${NAME} ${VERSION} --org http://github.com/mdavidsaver
-ibek support register ${NAME}
+    # declare packages for installation in the Dockerfile's runtime stage
+    ibek support add-runtime-packages libevent-dev
 
-# don't build test folders
-sed -i -E 's/(^[^#].*example)/# \1/' ${SUPPORT}/${NAME}/Makefile
-sed -i -E 's/(^[^#].*test)/# \1/' ${SUPPORT}/${NAME}/Makefile
+    # get the source and fix up the configure/RELEASE files
+    ibek support git-clone ${NAME} ${VERSION} --org http://github.com/mdavidsaver
+    ibek support register ${NAME}
 
-# declare the libs and DBDs that are required in ioc/iocApp/src/Makefile
-ibek support add-libs pvxs pvxsIoc
-ibek support add-dbds pvxsIoc.dbd
+    # don't build test folders
+    sed -i -E 's/(^[^#].*example)/# \1/' ${SUPPORT}/${NAME}/Makefile
+    sed -i -E 's/(^[^#].*test)/# \1/' ${SUPPORT}/${NAME}/Makefile
 
-# global config settings
-${FOLDER}/../_global/install.sh ${NAME}
+    # declare the libs and DBDs that are required in ioc/iocApp/src/Makefile
+    ibek support add-libs pvxs pvxsIoc
+    ibek support add-dbds pvxsIoc.dbd
 
-# compile the support module
-ibek support compile ${NAME}
+    # global config settings
+    ${FOLDER}/../_global/install.sh ${NAME}
+
+    # compile the support module
+    ibek support compile ${NAME}
+fi
