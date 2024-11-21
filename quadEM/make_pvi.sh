8dd5a91e47afae6a87c05fe2b93d91a936129ba7
Max Herbert <max.herbert@diamond.ac.uk>
2024-11-21 15:58:04 +0000

    Initial pvi changes

---
+++ quadEM/make_pvi.sh
@@ -0,0 +1,3 @@
+pvi convert device /workspaces/ioc-tetramm/ibek-support/quadEM  \
+    --header /epics/support/quadEM/quadEMApp/src/drvTetrAMM.h \
+    --template /epics/support/quadEM/db/quadEM.template
