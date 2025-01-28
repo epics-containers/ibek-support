d45347adfa4ad3402e537a7cd6209b97110ff1fe
Giles Knap <giles.knap@diamond.ac.uk>
2025-01-28 08:24:25 +0000

    switch to PCRE v8.45

--- StreamDevice/install.sh
+++ StreamDevice/install.sh
@@ -12,10 +12,15 @@ set -xe
 
 if [[ $EPICS_TARGET_ARCH == "RTEMS"* ]]; then
 
-    PCRE=7.5
     cd ${SUPPORT}/${NAME}
-    curl -LO https://sourceforge.net/projects/pcre/files/pcre/${PCRE}/pcre-${PCRE}.tar.gz
-    tar -xzf pcre-${PCRE}.tar.gz
+
+    # get the pcre source
+    PCRE=8.45
+    zip=https://sourceforge.net/projects/pcre/files/pcre/${PCRE}/pcre-${PCRE}.tar.gz
+    if [[ ! -d pcre-${PCRE} ]]; then
+        curl -LO ${zip}
+        tar -xzf pcre-${PCRE}.tar.gz
+    fi
     cd pcre-${PCRE}
 
     # set up cross-compilation
@@ -23,7 +28,11 @@ if [[ $EPICS_TARGET_ARCH == "RTEMS"* ]]; then
     export CXX=powerpc-rtems6-g++
     ./configure --host=powerpc-rtems6 --build=x86_64-linux-gnu \
         --prefix=${SUPPORT}/${NAME}/pcre-install
+    # remove build of executables - just build the library
+    sed -i -E 's/(^[^#].*PROGRAMS =.*$)/# \1/' Makefile
     make
+    make install
+    make clean
 
     # declare location of the pcre system library
     ibek support add-config-macro ${NAME} PCRE_LIB ${SUPPORT}/${NAME}/pcre-install/lib
