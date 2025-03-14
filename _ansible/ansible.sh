350e100c40c331ee785fdfa5d3aa6006cf88606f
Giles Knap <giles.knap@diamond.ac.uk>
2025-03-14 21:34:44 +0000

    Add support for ansible instead of install.sh scripts
    * Add ansible alternative to install.sh scripts
    
    add "Make a list of the RELEASE macros" NOT GOOD USE of ansible!
    
    release.yml fully working
    
    implement remove_macro from global RELEASE
    
    add first pass of config.yml
    
    improve build logging
    
    add RELEASE.local
    
    busy and asyn fully ansibled
    
    add iocStats.yml and patch file capability
    
    add softlinks and autosave
    
    add calc
    
    make paths in support module folders relative
    
    add mks937a
    
    make paths in support module folders relative
    
    add ADCore
    
    add ADSimDet and ffmpeg
    
    finalize ioc playbook
    
    * complete IOC playbook
    
    * ansible-lint passing
    
    * add playbook to install ALL support in Dockerfile
    
    * contrive to remove softlink back up to ibek-support
    
    * add ioc into the all playbook
    
    * add ansible lint to CI
    
    * fix global defs link
    
    * repair lint action
    
    * remove lint from CI for now
    
    * consolodate ansible bash scripts
    
    * remove redundant patch from iocStats
    
    * fix digitel lib and dbd
    
    * rename .yml to install.yml
    
    * tidy up names and folders
    
    * add schema for the *.install.yml files
    
    * reduce dependencies of ffmpegserver
    
    * improve ansible all playbook module discovery
    
    * add protocol_files and scripts features
    
    * support the all playbook for ibek-support AND ibek-support-dls etc. (PHEW!)
    
    * create the find role as 1st step in simplifying all role
    
    * simplify to one playbook
    
    * better guards in playbook.yml
    
    * fix iocStats for RTEMS
    
    * dont include ioc in all_support

---
+++ _ansible/ansible.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# launch script for the ansible playbooks
+
+module_name=$1
+module_version=$2
+
+if [[ -z $module_name ]]; then
+    echo "Usage: $0 <module_name> [<module_version>]"
+    echo "  where <module_name> the support module to install, or 'ioc' or 'all'"
+    echo
+    echo "  launches an ansible playbook to configure and install the module[s]"
+    exit 1
+fi
+
+if [[ -n $module_version ]]; then
+    vers="-e version=${module_version}"
+fi
+
+if [[ $ANSIBLE_TAGS ]]; then
+    tags=" --tags ${ANSIBLE_TAGS}"
+    echo "limiting ansible run to tags: ${ANSIBLE_TAGS}"
+fi
+
+if [[ $ANSIBLE_ARGS ]]; then
+    args="${ANSIBLE_ARGS}"
+fi
+
+# ansible playbook and roles come from the ibek-support repo always
+ansible_dir=/epics/generic-source/ibek-support/_ansible
+pb=${ansible_dir}/playbook.yml
+
+vars=" -e module_name=${module_name}"
+
+set -x
+ansible-playbook ${pb} -i ${ansible_dir}/hosts.yml ${vars} ${vers} ${tags} ${args}
