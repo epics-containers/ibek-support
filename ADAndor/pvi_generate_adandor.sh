e2b395e4cacffe09ebf45c727ccf17c8cf56ffea
Gregory Gay <gregory.gay@diamond.ac.uk>
2026-04-02 10:57:00 +0000

    update pvi definitions for ADAndor device
    Shamrock template not included
    because pvi missing asynFloat32ArrayIn waveform support

--- ADAndor/pvi_generate_adandor.sh
+++ ADAndor/pvi_generate_adandor.sh
@@ -23,8 +23,9 @@ pvi convert device /workspaces/ioc-adandor/ibek-support/ADAndor  \
     --name ADAndor \
     --header /epics/support/ADAndor/andorApp/src/andorCCD.h \
     --template /epics/support/ADAndor/andorApp/Db/andorCCD.template \
-    --template /epics/support/ADAndor/andorApp/Db/shamrock.template
+    # pvi definition is missing waveform conversion for records with 'asynFloat32ArrayIn' DTYP
+    # --template /epics/support/ADAndor/andorApp/Db/shamrock.template
 
-pvi regroup ADAndor.pvi.device.yaml ${ADANDOR}/andorApp/op/adl/*.adl
+pvi regroup ADAndor/ADAndor.pvi.device.yaml ${ADANDOR}/andorApp/op/adl/*.adl
 
 ibek support generate-links $(pwd)
