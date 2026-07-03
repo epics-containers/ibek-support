d3449cc521e9fde65ae48007905abf7912f8906e
Gregory Gay <gregory.gay@diamond.ac.uk>
2026-07-03 11:15:27 +0000

    update pvi.device.yaml for ADAndor

--- ADAndor/pvi_generate_adandor.sh
+++ ADAndor/pvi_generate_adandor.sh
@@ -23,8 +23,8 @@ pvi convert device /workspaces/ioc-adandor/ibek-support/ADAndor  \
     --name ADAndor \
     --header /epics/support/ADAndor/andorApp/src/andorCCD.h \
     --template /epics/support/ADAndor/andorApp/Db/andorCCD.template \
+    # --template /epics/support/ADAndor/andorApp/Db/shamrock.template \
     # pvi definition is missing waveform conversion for records with 'asynFloat32ArrayIn' DTYP
-    # --template /epics/support/ADAndor/andorApp/Db/shamrock.template
 
 pvi regroup ADAndor/ADAndor.pvi.device.yaml ${ADANDOR}/andorApp/op/adl/*.adl
 
